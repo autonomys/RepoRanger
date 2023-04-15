2e099125930d5f097f6eb19107bf7bd60cd42836
Serge Kovbasiuk <isserge@protonmail.com>
2023-04-15 18:13:08 +0200

    add copy button

--- src/SelectedFiles.tsx
+++ src/SelectedFiles.tsx
@@ -34,26 +34,41 @@ export const SelectedFiles: React.FC<{
     };
   }, [selectedFiles, repo]);
 
+  const handleCopy = () => {
+    const content = [...selectedFileContents.values()].join('\n\n');
+    navigator.clipboard.writeText(content).then(() => {
+      alert('Contents copied to clipboard.');
+    });
+  };
+
   return (
     <div>
-      <h2>Selected Files:</h2>
-      <pre>
-        {[...selectedFiles].map((path, index) => {
-          const file = files.find((f) => f.path === path);
-          if (file) {
-            return (
-              <>
-                <h3>
-                  {index + 1}. file: {file.path}
-                </h3>
-                <p>{selectedFileContents.get(path)}</p>
-              </>
-            );
-          } else {
-            return null;
-          }
-        })}
-      </pre>
+      <div className="flex justify-between items-center mb-4">
+        <h2>Selected Files:</h2>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleCopy}
+        >
+          Copy Contents
+        </button>
+      </div>
+    <pre>
+      {[...selectedFiles].map((path, index) => {
+        const file = files.find((f) => f.path === path);
+        if (file) {
+          return (
+            <>
+              <h3>
+                {index + 1}. file: {file.path}
+              </h3>
+              <p>{selectedFileContents.get(path)}</p>
+            </>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </pre>
     </div>
   );
 };
