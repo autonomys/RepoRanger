1e4c1777b10ed2cfd0e998f2f441ac1ba10c502c
Serge Kovbasiuk <isserge@protonmail.com>
2023-04-16 16:06:22 +0200

    adjust LastCommit styles

--- src/components/LastCommit.tsx
+++ src/components/LastCommit.tsx
@@ -11,9 +11,11 @@ interface LastCommitProps {
 export const LastCommit: React.FC<LastCommitProps> = ({ commit }) => {
   const timestamp = new Date(commit.timestamp).toLocaleString();
   return (
-    <div className="flex flex-col mt-4">
+    <div className="last-commit-container mt-6 mb-4">
       <div className="text-gray-500 text-sm">Last Commit:</div>
-      <div className="text-gray-700 font-medium">{commit.hash}</div>
+      <div className="text-gray-700 font-medium text-sm md:text-base">
+        {commit.hash}
+      </div>
       <div className="text-gray-500 text-sm">{commit.message}</div>
       <div className="text-gray-500 text-sm">{timestamp}</div>
     </div>
