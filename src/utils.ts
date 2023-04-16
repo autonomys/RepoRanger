d8da1c9545a6d38285d3487dea20288606012178
Serge Kovbasiuk <isserge@protonmail.com>
2023-04-16 18:09:31 +0200

    Add char limit message

--- src/utils.ts
+++ src/utils.ts
@@ -27,3 +27,8 @@ export const sortFilesBySelection = (
     return bSelected - aSelected;
   });
 };
+
+export function formatNumber(number: number): string {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+}
+
