8df4b1e5ad5f9dc6d88b19a34ce03a0ee2883e82
Serge Kovbasiuk <isserge@protonmail.com>
2023-04-23 16:27:53 +0200

    move reducer to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { GitHubFile } from './types';
 import {
   RepositoryInput,
   Result,
-  BranchSelector,
+  Branches,
   Loading,
   NoRepositorySelected,
   FileList,
@@ -17,95 +17,7 @@ import {
   FileFilter,
   Header,
 } from './components';
-
-interface State {
-  files: GitHubFile[];
-  selectedFiles: Set<string>;
-  repo: string;
-  isLoadingRepoFiles: boolean;
-  isLoadingFileContents: boolean;
-  selectedBranch: string;
-  branches: {
-    name: string;
-    lastCommit: { hash: string; message: string; timestamp: string };
-  }[];
-  searchQuery: string;
-  fileExtensions: string[];
-  selectedExtensions: string[];
-}
-
-export type Action =
-  | { type: 'RESET' }
-  | { type: 'SET_FILES'; payload: GitHubFile[] }
-  | { type: 'SET_SELECTED_FILES'; payload: Set<string> }
-  | { type: 'SET_REPO'; payload: string }
-  | { type: 'SET_IS_LOADING_REPO_FILES'; payload: boolean }
-  | { type: 'SET_IS_LOADING_FILE_CONTENTS'; payload: boolean }
-  | { type: 'SET_SELECTED_BRANCH'; payload: string }
-  | {
-      type: 'SET_BRANCHES';
-      payload: Array<{
-        name: string;
-        lastCommit: { hash: string; message: string; timestamp: string };
-      }>;
-    }
-  | { type: 'SET_SELECTED_FILE_EXTENSION'; payload: string[] }
-  | { type: 'SET_SEARCH_QUERY'; payload: string }
-  | { type: 'SET_FILE_EXTENSIONS'; payload: string[] }
-  | { type: 'CLEAR_SELECTED_FILES' };
-
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case 'SET_FILES':
-      return { ...state, files: action.payload };
-    case 'SET_SELECTED_FILES':
-      return { ...state, selectedFiles: action.payload };
-    case 'SET_REPO':
-      return { ...state, repo: action.payload };
-    case 'SET_IS_LOADING_REPO_FILES':
-      return { ...state, isLoadingRepoFiles: action.payload };
-    case 'SET_IS_LOADING_FILE_CONTENTS':
-      return { ...state, isLoadingFileContents: action.payload };
-    case 'SET_SELECTED_BRANCH':
-      return { ...state, selectedBranch: action.payload };
-    case 'SET_BRANCHES':
-      return { ...state, branches: action.payload };
-    case 'RESET':
-      return { ...initialState };
-    case 'SET_SELECTED_FILE_EXTENSION':
-      return {
-        ...state,
-        selectedExtensions: action.payload,
-      };
-    case 'SET_SEARCH_QUERY':
-      return {
-        ...state,
-        searchQuery: action.payload,
-      };
-    case 'SET_FILE_EXTENSIONS':
-      return {
-        ...state,
-        fileExtensions: action.payload,
-      };
-    case 'CLEAR_SELECTED_FILES':
-      return { ...state, selectedFiles: new Set() };
-    default:
-      return state;
-  }
-};
-
-const initialState: State = {
-  files: [],
-  selectedFiles: new Set(),
-  repo: '',
-  isLoadingRepoFiles: false,
-  isLoadingFileContents: false,
-  selectedBranch: '',
-  branches: [],
-  selectedExtensions: [],
-  searchQuery: '',
-  fileExtensions: [],
-};
+import { reducer, initialState } from './stateReducer';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -155,7 +67,7 @@ function App() {
     };
   }, [repo, selectedBranch]);
 
-  const handleRepoSubmit = async (repo: string) => {
+  const handleRepoSubmit = useCallback(async (repo: string) => {
     try {
       const branches = await fetchBranches(repo).then((branches) =>
         branches.sort((a, b) => {
@@ -172,48 +84,73 @@ function App() {
     } catch (error) {
       alert('Failed to fetch repository branches');
     }
-  };
+  }, []);
 
-  const handleSelection = (file: GitHubFile) => {
-    const files = getSelectedFiles(selectedFiles, file);
-    dispatch({
-      type: 'SET_SELECTED_FILES',
-      payload: files,
-    });
-  };
-
-  const handleReset = () => {
-    dispatch({ type: 'RESET' });
-  };
-
-  const selectedBranchItem = branches.find(
-    (branch) => branch.name === selectedBranch
+  const handleSelection = useCallback(
+    (file: GitHubFile) => {
+      const files = getSelectedFiles(selectedFiles, file);
+      dispatch({
+        type: 'SET_SELECTED_FILES',
+        payload: files,
+      });
+    },
+    [selectedFiles]
   );
 
-  const handleBranchSelect = (branch: string) => {
+  const handleReset = useCallback(() => {
+    dispatch({ type: 'RESET' });
+  }, []);
+
+  const handleBranchSelect = useCallback((branch: string) => {
     dispatch({ type: 'SET_SELECTED_BRANCH', payload: branch });
-  };
+  }, []);
 
-  const handleSelectFileExtension = (extension: string) => {
-    if (selectedExtensions.includes(extension)) {
-      dispatch({
-        type: 'SET_SELECTED_FILE_EXTENSION',
-        payload: selectedExtensions.filter((ext) => ext !== extension),
-      });
-    } else {
-      dispatch({
-        type: 'SET_SELECTED_FILE_EXTENSION',
-        payload: [...selectedExtensions, extension],
-      });
-    }
-  };
+  const handleSelectFileExtension = useCallback(
+    (extension: string) => {
+      if (selectedExtensions.includes(extension)) {
+        dispatch({
+          type: 'SET_SELECTED_FILE_EXTENSION',
+          payload: selectedExtensions.filter((ext) => ext !== extension),
+        });
+      } else {
+        dispatch({
+          type: 'SET_SELECTED_FILE_EXTENSION',
+          payload: [...selectedExtensions, extension],
+        });
+      }
+    },
+    [selectedExtensions]
+  );
 
-  const handleSearchQuery = (extension: string) => {
+  const handleSearchQuery = useCallback((extension: string) => {
     dispatch({
       type: 'SET_SEARCH_QUERY',
       payload: extension,
     });
-  };
+  }, []);
+
+  const handleClear = useCallback(() => {
+    dispatch({
+      type: 'SET_SELECTED_FILE_EXTENSION',
+      payload: [],
+    });
+    dispatch({
+      type: 'SET_SEARCH_QUERY',
+      payload: '',
+    });
+  }, []);
+
+  const handleClearFiles = useCallback(() => {
+    dispatch({ type: 'CLEAR_SELECTED_FILES' });
+  }, []);
+
+  const setContentsLoading = useCallback((isLoading: boolean) => {
+    dispatch({ type: 'SET_IS_LOADING_FILE_CONTENTS', payload: isLoading });
+  }, []);
+
+  const selectedBranchItem = branches.find(
+    (branch) => branch.name === selectedBranch
+  );
 
   const displayedFiles = useMemo(() => {
     let filesToDisplay = files;
@@ -231,25 +168,6 @@ function App() {
     );
   }, [files, selectedExtensions, selectedFiles, searchQuery]);
 
-  const handleClear = () => {
-    dispatch({
-      type: 'SET_SELECTED_FILE_EXTENSION',
-      payload: [],
-    });
-    dispatch({
-      type: 'SET_SEARCH_QUERY',
-      payload: '',
-    });
-  };
-
-  const handleClearFiles = () => {
-    dispatch({ type: 'CLEAR_SELECTED_FILES' });
-  };
-
-  const setContentsLoading = useCallback((isLoading: boolean) => {
-    dispatch({ type: 'SET_IS_LOADING_FILE_CONTENTS', payload: isLoading });
-  }, []);
-
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
@@ -261,10 +179,10 @@ function App() {
               onReset={handleReset}
             />
             {repo && (
-              <BranchSelector
+              <Branches
                 branches={branches}
                 selectedBranch={selectedBranch}
-                onBranchChange={handleBranchSelect}
+                handleBranchSelect={handleBranchSelect}
               />
             )}
             {selectedBranchItem && (
---
+++ src/stateReducer.ts
@@ -0,0 +1,90 @@
+import { GitHubFile } from './types';
+
+interface State {
+  files: GitHubFile[];
+  selectedFiles: Set<string>;
+  repo: string;
+  isLoadingRepoFiles: boolean;
+  isLoadingFileContents: boolean;
+  selectedBranch: string;
+  branches: {
+    name: string;
+    lastCommit: { hash: string; message: string; timestamp: string };
+  }[];
+  searchQuery: string;
+  fileExtensions: string[];
+  selectedExtensions: string[];
+}
+
+export type Action =
+  | { type: 'RESET' }
+  | { type: 'SET_FILES'; payload: GitHubFile[] }
+  | { type: 'SET_SELECTED_FILES'; payload: Set<string> }
+  | { type: 'SET_REPO'; payload: string }
+  | { type: 'SET_IS_LOADING_REPO_FILES'; payload: boolean }
+  | { type: 'SET_IS_LOADING_FILE_CONTENTS'; payload: boolean }
+  | { type: 'SET_SELECTED_BRANCH'; payload: string }
+  | {
+      type: 'SET_BRANCHES';
+      payload: Array<{
+        name: string;
+        lastCommit: { hash: string; message: string; timestamp: string };
+      }>;
+    }
+  | { type: 'SET_SELECTED_FILE_EXTENSION'; payload: string[] }
+  | { type: 'SET_SEARCH_QUERY'; payload: string }
+  | { type: 'SET_FILE_EXTENSIONS'; payload: string[] }
+  | { type: 'CLEAR_SELECTED_FILES' };
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'SET_FILES':
+      return { ...state, files: action.payload };
+    case 'SET_SELECTED_FILES':
+      return { ...state, selectedFiles: action.payload };
+    case 'SET_REPO':
+      return { ...state, repo: action.payload };
+    case 'SET_IS_LOADING_REPO_FILES':
+      return { ...state, isLoadingRepoFiles: action.payload };
+    case 'SET_IS_LOADING_FILE_CONTENTS':
+      return { ...state, isLoadingFileContents: action.payload };
+    case 'SET_SELECTED_BRANCH':
+      return { ...state, selectedBranch: action.payload };
+    case 'SET_BRANCHES':
+      return { ...state, branches: action.payload };
+    case 'RESET':
+      return { ...initialState };
+    case 'SET_SELECTED_FILE_EXTENSION':
+      return {
+        ...state,
+        selectedExtensions: action.payload,
+      };
+    case 'SET_SEARCH_QUERY':
+      return {
+        ...state,
+        searchQuery: action.payload,
+      };
+    case 'SET_FILE_EXTENSIONS':
+      return {
+        ...state,
+        fileExtensions: action.payload,
+      };
+    case 'CLEAR_SELECTED_FILES':
+      return { ...state, selectedFiles: new Set() };
+    default:
+      return state;
+  }
+};
+
+export const initialState: State = {
+  files: [],
+  selectedFiles: new Set(),
+  repo: '',
+  isLoadingRepoFiles: false,
+  isLoadingFileContents: false,
+  selectedBranch: '',
+  branches: [],
+  selectedExtensions: [],
+  searchQuery: '',
+  fileExtensions: [],
+};
